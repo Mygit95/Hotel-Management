7c4a3932c0cec85cb9df040b200e53ac55b16bd2
Ashish Ahiwale <ahiwaleashish1995@gmail.com>
2024-02-20 20:30:02 +0530

    hotel management system

--- hotel-booking-system/src/main/java/com/hotelbookingsystem/project/Controller/BookingController.java
+++ hotel-booking-system/src/main/java/com/hotelbookingsystem/project/Controller/BookingController.java
@@ -5,10 +5,12 @@ import com.hotelbookingsystem.project.Models.Request.CancelBooking;
 import com.hotelbookingsystem.project.Models.Response.AvailableRooms;
 import com.hotelbookingsystem.project.Models.Response.UserBookings;
 import com.hotelbookingsystem.project.Service.BookingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class BookingController {
 
     private final BookingService bookingService;
@@ -19,26 +21,32 @@ public class BookingController {
 
     @GetMapping("/rooms")
     private ResponseEntity<AvailableRooms> getAvailableRooms(@RequestHeader(required = false) String userId) {
-        return ResponseEntity.ok(bookingService.getAvailableRooms(userId));
+        log.info("Get Available Rooms, userId: {}", userId);
+        AvailableRooms response = bookingService.getAvailableRooms(userId);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/book")
     private ResponseEntity<String> bookRoom(@RequestHeader Long userId, @RequestBody BookingRequest bookingRequest) {
+        log.info("Book a room, userId: {}", userId);
         return ResponseEntity.ok(bookingService.bookRoom(userId, bookingRequest));
     }
 
     @GetMapping("/activeBookings")
     private ResponseEntity<UserBookings> getAvailableRooms(@RequestHeader Long userId) {
+        log.info("Get active bookings, userId: {}", userId);
         return ResponseEntity.ok(bookingService.getActiveBookings(userId));
     }
 
     @GetMapping("/bookingHistory")
     private ResponseEntity<UserBookings> getBookingHistory(@RequestHeader Long userId) {
+        log.info("Get Booking History, userId: {}", userId);
         return ResponseEntity.ok(bookingService.getBookingHistory(userId));
     }
 
     @DeleteMapping("/cancelBooking")
     private ResponseEntity<String> cancelBooking(@RequestHeader Long userId, @RequestBody CancelBooking cancelBooking) {
+        log.info("Cancel Bookings, userId: {}", userId);
         return ResponseEntity.ok(bookingService.CancelBooking(userId, cancelBooking.getBookingId()));
     }
 }
